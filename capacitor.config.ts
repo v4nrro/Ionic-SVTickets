4822bb0318bc65a7f85743d4d1a30f27c9af9669
v4nrro <ivannavarro0105@gmail.com>
2025-01-27 08:51:58 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ionic-SVTickets',
+  webDir: 'www'
+};
+
+export default config;
